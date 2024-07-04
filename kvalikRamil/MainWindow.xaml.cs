ad1615e861aa700fa4335fa0445ba7996f16fdce
User01 <garaeva-r_23@mail.ru>
2024-07-04 10:22:45 +0300

    Добавьте файлы проекта.

---
+++ kvalikRamil/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kvalikRamil
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Действия при нажатии на текст
+            MessageBox.Show("Текст был нажат!");
+        }
+
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "Введите для поиска")
+            {
+                textBox.Text = "";
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Введите для поиска";
+                textBox.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void ComboBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            if (comboBox.SelectedIndex == 0)
+            {
+                comboBox.SelectedIndex = -1;
+            }
+            ComboBox comboBoxi = (ComboBox)sender;
+            if (comboBox.SelectedIndex == 0)
+            {
+                comboBox.SelectedIndex = -1;
+            }
+        }
+
+        private void ComboBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            if (comboBox.SelectedIndex == -1)
+            {
+                comboBox.SelectedIndex = 0;
+            }
+            ComboBox comboBoxi = (ComboBox)sender;
+            if (comboBox.SelectedIndex == -1)
+            {
+                comboBox.SelectedIndex = 0;
+            }
+        }
+    }
+}
